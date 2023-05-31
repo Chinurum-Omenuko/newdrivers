bd691e766c0a6dc85536f19ab9257f89d0e6cb4f
Chinurum-Omenuko <omen0003@algonquinlive.com>
2023-05-31 09:42:49 -0400

    Styles Navbar with med.q and inclding burger

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 import './Navbar.css'
+import Burger from '../../assets/images/burger.svg'
 
 function Navbar() {
     return(
-    <nav class="flex flex-row fixed justify-between w-full px-20 py-5">
+    <nav class="flex flex-row justify-between">
         <div class="logo">
             <a href="./index.html">Chinurum Omenuko</a>
         </div>
-        <ul class="[&>a]:m-10">
+        <ul>
             <a href="http://">About</a>
             <a href="http://">Projects</a>
             <a href="http://">Contact</a>
         </ul>
+        <div className="hamburger-icon">
+            <img src={Burger} alt="" srcset="" />
+        </div>
     </nav>
     );
 }
