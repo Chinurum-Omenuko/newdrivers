2c9ed15c16b2f6c594a4915236c9f6034da7562b
Chinurum-Omenuko <omen0003@algonquinlive.com>
2023-06-01 21:20:08 -0400

    renamed icons and rewrote html

--- my-portfolio/src/pages/Site.jsx
+++ my-portfolio/src/pages/Site.jsx
@@ -3,6 +3,7 @@ import '.././assets/stylesheets/Site.css'
 
 import Navbar from '../components/Navbar.jsx';
 
+
 function Site() {
   return(
     <div class="wrapper border-blue-500 border-2">
@@ -15,12 +16,12 @@ function Site() {
 
 function Home() {
   return(
-  <main class="bg-container flex justify-center border-2 border-yellow-100">
-    <div class="content-wrapper w-full ">
+  <main class="bg-container flex justify-center border-2 border-yellow-800">
+    <div class="content-wrapper w-full main-wrapper">
       <div class="text-left headings">
         <h1>Chinurum Omenuko</h1>
         <h2>Portfolio Website</h2>
-        <p>Front End Developer from Ontario, Canada</p>
+        <p>Front End Developer</p>
       </div>
 
       <div class="icons m-10 flex flex-col justify-center">
@@ -28,8 +29,8 @@ function Home() {
           <button class="">Download CV</button>
         </div>
         <div class="links flex flex-row justify-center my-5">
-          <a href="https://github.com/Chinurum-Omenuko/Chinurum-Omenuko.github.io/"></a>
-          <a href="https://github.com/Chinurum-Omenuko/Chinurum-Omenuko.github.io/"></a>
+          <a href="https://github.com/Chinurum-Omenuko/Chinurum-Omenuko.github.io/"><img src="src/assets/images/github.png" alt="" srcset="" /></a>
+          <a href="https://github.com/Chinurum-Omenuko/Chinurum-Omenuko.github.io/"><img src="src/assets/images/linkedin-64.png" alt="" srcset="" /></a>
         </div>   
       </div>
 
