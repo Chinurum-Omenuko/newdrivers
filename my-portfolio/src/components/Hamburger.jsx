bd0df6a16086212a083c70588cbd8f362c047215
Chinurum-Omenuko <omen0003@algonquinlive.com>
2023-06-09 17:35:54 -0400

    Creates and Adds Hamburger to Navbar comp

---
+++ my-portfolio/src/components/Hamburger.jsx
@@ -0,0 +1,15 @@
+import '.././assets/stylesheets/Hamburger.css'
+
+function Hamburger(){
+    return(
+    <div className="nav-menu">
+        <ul className="menu-list">
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    );
+}
+
+export default Hamburger
