4256e4ec90394b7701aa3a29816e9696bccbbc17
Chinurum-Omenuko <omen0003@algonquinlive.com>
2023-06-29 16:17:58 -0400

    adds navdrawer components to app.jsx

--- newdrivers/src/components/Navigation/Mobilenav/Mobilenav.jsx
+++ newdrivers/src/components/Navigation/Mobilenav/Mobilenav.jsx
@@ -2,7 +2,14 @@ import './Mobilenav.css'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import logo from '../../../assets/logo/logo.svg'
 
+import { useState } from 'react';
+
+
+
+
 function Mobilenav() {
+    const [isClicked, showDrawer] = useState(false)
+    
     return ( 
     <div className="mnav">
         <div className="logo-container flex flex-row items-center">
@@ -10,6 +17,7 @@ function Mobilenav() {
             <h3>New Drivers</h3>
         </div>
         <GiHamburgerMenu className='hamburger' size={30}/>
+
     </div> 
     );
 }
---
+++ newdrivers/src/components/Navigation/Navdrawer/Navdrawer.jsx
@@ -0,0 +1,19 @@
+import './Navdrawer.css'
+
+function Navdrawer() {
+    return ( 
+    <div className="navdrawer">
+        <div className="wrapper">
+            <div className='drawer-items'>
+                <a href="">Home</a>
+                <a href="">Plans</a>``
+                <a href="">Courses</a>
+                <a href="">About</a>
+                <a href="">Contacts</a>
+            </div>
+        </div>
+    </div> 
+    );
+}
+
+export default Navdrawer;
